470ca060cf4431ecae1bd55ce694e0b6429f618d
Karan Babar <karan.babar2004@gmail.com>
2025-12-19 12:26:58 +0530

    Reduce time complexity of bubble sort.

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,15 +2,30 @@ package Sorting;
 
 public class BubbleSort {
     public static void sort(int arr[]){
+        int swaps = 0;
+
       for(int turn=0; turn<arr.length-1; turn++){
+        boolean swapped = false;
+
         for(int j=0; j<arr.length-1-turn; j++){
             if(arr[j] > arr[j+1]){
                 //swap
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+
+                swaps++;
+                swapped = true;
             }
         }
+        if(!swapped){
+            break;
+        }
+      }
+      System.out.println("total swaps = " + swaps);
+
+      if (swaps == 0) {
+        System.out.println("Array is already sorted");
       }
     }
 
@@ -21,7 +36,7 @@ public class BubbleSort {
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[] = {3,5,2,1,4};
+        int arr[] = {1,2,3,4,5};
         sort(arr);
         printArr(arr);
     }
