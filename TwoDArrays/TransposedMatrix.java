a4b00f19c562b5c4145892b9c1643a73009a9a49
Karan Babar <karan.babar2004@gmail.com>
2025-12-20 22:29:23 +0530

    Transpose matrix in 2D Array.

---
+++ TwoDArrays/TransposedMatrix.java
@@ -0,0 +1,29 @@
+package TwoDArrays;
+
+public class TransposedMatrix {
+    
+    public static void printTranspose(int matrix[][]){
+
+        for(int i=0; i<matrix.length; i++){
+        for(int j=0; j<matrix[0].length; j++){
+            System.out.print(matrix[i][j] + " ");
+        }
+        System.out.println();
+      }
+
+       // Transpose matrix
+        System.out.println("\nTransposed Matrix:");
+      for (int j = 0; j < matrix[0].length; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{4,7,8},
+                          {8,8,7}};
+
+        printTranspose(matrix);                
+    }
+}
