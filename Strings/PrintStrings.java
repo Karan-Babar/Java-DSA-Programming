9c33063b69041b90584d48cd841b623b1ca52132
Karan Babar <karan.babar2004@gmail.com>
2025-12-21 19:42:02 +0530

    Concatenation in strings.

--- Strings/PrintStrings.java
+++ Strings/PrintStrings.java
@@ -9,9 +9,15 @@ public class PrintStrings {
         // String name = "Karan";
         // System.out.println(name);
 
+        //Concatenation
+        String firstName = "karan";
+        String lastName = "babar";
+        String fullName = firstName+" "+lastName;
+        System.out.println(fullName);
+
         Scanner sc = new Scanner(System.in);
 
-        String name1 = sc.nextLine();
+        String name1 = sc.nextLine(); //Line used for print two or more words in string.
         System.out.println(name1);
     }
 }
