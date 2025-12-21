c9672495b6c90ed9e8498193d08dadee968f6afd
Karan Babar <karan.babar2004@gmail.com>
2025-12-21 19:30:25 +0530

    New topic Strings.

---
+++ Strings/PrintStrings.java
@@ -0,0 +1,17 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class PrintStrings {
+
+    public static void main(String[] args){
+         
+        // String name = "Karan";
+        // System.out.println(name);
+
+        Scanner sc = new Scanner(System.in);
+
+        String name1 = sc.nextLine();
+        System.out.println(name1);
+    }
+}
