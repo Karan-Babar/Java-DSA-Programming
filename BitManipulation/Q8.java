b18a9715b2693813dfeef5d278597be553414eb0
Karan Babar <karan.babar2004@gmail.com>
2025-12-27 19:44:28 +0530

    Practice question.

---
+++ BitManipulation/Q8.java
@@ -0,0 +1,14 @@
+package BitManipulation;
+
+public class Q8 {
+    
+     //Add 1 to an integer using bit manipulation
+     public static void main(String[] args) {
+        int x = 5;
+
+        int result = - (~x);
+
+        System.out.println("Original number: " + x);
+        System.out.println("After adding 1: " + result);
+    }
+}
