98a86552e142e4807a6bab14470b48718017e288
Karan Babar <karan.babar2004@gmail.com>
2025-12-29 10:42:03 +0530

    Destructors in OOPs.

---
+++ OOPs/Destructors.java
@@ -0,0 +1,8 @@
+package OOPs;
+
+public class Destructors {
+    
+    //Java does not have the concept of a destructor like in C++. 
+    // Instead, Java uses automatic garbage collection to manage memory, 
+    // which automatically reclaims memory from objects that are no longer referenced. 
+}
