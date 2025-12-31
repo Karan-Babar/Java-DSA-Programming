5120b89e39af46271dc1d70a3d19219c36ff3951
Karan Babar <karan.babar2004@gmail.com>
2025-12-31 22:18:59 +0530

    Recursion problem 1.

--- Recursion/Problem1.java
+++ Recursion/Problem1.java
@@ -16,10 +16,25 @@ public class Problem1 {
          System.out.print(n+" ");
          printDec(n-1);
     }
+    
+
+    //Print numbers ftom n to 1 (Increasing Order)
+    public static void printInc(int n){
+         if(n == 1){
+            System.out.print(n+" ");
+            return;
+         }
+
+         printInc(n-1);
+         System.out.print(n+" ");
+        
+    }
 
     public static void main(String[] args) {
         int n = 10;
 
         printDec(n);
+        System.out.println();
+        printInc(n);
     }
 }
