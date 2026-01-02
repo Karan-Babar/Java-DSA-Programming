c354aacfa12089f2c2eecebac58abd2477de2d1d
Karan Babar <karan.babar2004@gmail.com>
2026-01-02 21:06:11 +0530

    Optimized Solution.

--- Recursion/Problem5.java
+++ Recursion/Problem5.java
@@ -3,6 +3,7 @@ package Recursion;
 //Print x to the power n.
 public class Problem5 {
     
+    //O(n)
     public static int power(int x, int n){
         if(n == 0){
             return 1;
@@ -14,8 +15,26 @@ public class Problem5 {
 
         return x * power(x, n-1);
     }
+
+    //Optimized O(logn)
+    public static int optimizedPower(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+         
+        int halfPower = optimizedPower(x, n/2);
+        int halfPowerSq = halfPower * halfPower;
+
+        //n is odd
+        if(n % 2 != 0){
+            halfPowerSq = x * halfPowerSq;
+        }
+
+        return halfPowerSq;
+    }
     public static void main(String[] args) {
         
         System.out.println(power(2, 10));
+        System.out.println(optimizedPower(2, 5));
     }
 }
