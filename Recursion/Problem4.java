ede1bf122e2e14d05c382c59c299fbfc72a39a42
Karan Babar <karan.babar2004@gmail.com>
2026-01-02 20:34:03 +0530

    Practice question.

--- Recursion/Problem4.java
+++ Recursion/Problem4.java
@@ -13,10 +13,25 @@ public class Problem4 {
         }
         return findKey(arr, key, i+1);
     }
+
+    //find the Last occurence of an element in an array.
+    public static int findLastKey(int arr[], int key, int i){
+     if(i == arr.length){
+        return -1;
+     }
+     int isFound = findLastKey(arr, key, i+1);
+     if(isFound == -1 && arr[i] == key){
+        return i;
+     }
+
+     return isFound;
+    }
+
     public static void main(String[] args) {
         int arr[] = {3,2,5,7,5};
         int key = 5;
 
         System.out.println(findKey(arr, key,0));
+        System.out.println(findLastKey(arr, key,0));
     }
 }
