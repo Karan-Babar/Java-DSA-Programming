b1d8c59e04db29cb739c8f61f32d9ca838c9fccc
Karan Babar <karan.babar2004@gmail.com>
2026-01-06 19:57:20 +0530

    Binary Strings Problem.

---
+++ Recursion/Problem9.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+//Binary Strings Problem
+//Print all binary strings of size N without consecutive ones.
+//like 000 , 001, 101, 100, 010.
+public class Problem9 {
+    
+    public static void printBinStrings(int n, int lastPlace, String str){
+
+        //base case
+        if(n == 0) {
+            System.out.println(str);
+            return;
+        }
+
+        //kaam
+        printBinStrings(n-1, 0, str+"0");
+
+        if(lastPlace == 0){
+            printBinStrings(n-1, 1, str+"1");
+        }
+    }
+    public static void main(String[] args) {
+        
+        printBinStrings(3, 0, "");
+    }
+}
