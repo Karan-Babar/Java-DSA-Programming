17e846a98aa6ea193439dde857dc36af4711efe9
Karan Babar <karan.babar2004@gmail.com>
2026-01-19 22:59:29 +0530

    Subsets using Backtracking.

---
+++ Backtracking/Subsets.java
@@ -0,0 +1,28 @@
+package Backtracking;
+
+public class Subsets {
+   
+    //Time complexity O((2^n) * n)
+    public static void findSubsets(String str, String ans, int i){
+        //bace case
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+
+            return;
+        }
+
+        //yes choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //No choice
+        findSubsets(str, ans, i+1);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+
+        findSubsets(str, "", 0);
+    }
+}
