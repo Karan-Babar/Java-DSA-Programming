74eb9ed786500121a50b957c9e0c1a04be73a0a9
Karan Babar <karan.babar2004@gmail.com>
2026-01-19 21:05:52 +0530

    Backtracking on Arrays.

---
+++ Backtracking/Arrays.java
@@ -0,0 +1,29 @@
+package Backtracking;
+
+public class Arrays {
+    
+    public static void addValue(int arr[], int i, int val){
+       //base
+        if(i == arr.length){
+            printArray(arr);
+            return;
+        }
+        //recursion
+        arr[i] = val;
+        addValue(arr, i+1, val+1);
+        arr[i] = arr[i] - 2;
+    }
+
+    public static void printArray(int arr[]){
+       for(int i=0; i<arr.length; i++){
+         System.out.print(arr[i]+" ");
+       }
+       System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+
+        addValue(arr, 0, 1);
+        printArray(arr);
+    }
+}
