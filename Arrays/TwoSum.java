f2df3eae9cd5efff209821b5e661438167412a63
Karan Babar <karan.babar2004@gmail.com>
2026-02-20 23:31:12 +0530

    Two sum New Approach

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,26 +1,51 @@
 package Arrays;
 
-public class TwoSum {
-    public static void twoSum(int[] nums, int target) {
-    int count = 0;
-    for (int i = 0; i < nums.length; i++) {
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                System.out.println("("+i+","+j+")");
-                count++;
-            }
-        }
-    }
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-    if (count == 0) {
-            System.out.println("Target not found");
+public class TwoSum {
+
+    //O(n^2)
+    // public static void twoSum(int[] nums, int target) {
+    // int count = 0;
+    // for (int i = 0; i < nums.length; i++) {
+    //     for (int j = i + 1; j < nums.length; j++) {
+    //         if (nums[i] + nums[j] == target) {
+    //             System.out.println("("+i+","+j+")");
+    //             count++;
+    //         }
+    //     }
+    // }
+
+    // if (count == 0) {
+    //         System.out.println("Target not found");
+    //     }
+    // }
+
+    //O(n)
+     public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int remaining = target - nums[i];
+            
+            if (mp.containsKey(remaining)) {
+                return new int[]{mp.get(remaining), i};
+            }
+            
+            mp.put(nums[i], i);
         }
+
+        return new int[]{};
     }
 
     public static void main(String[] args) {
         int nums[] = {2,3,7,9,3};
-        int target = 7;
+        int target = 9;
 
-        twoSum(nums, target);
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
     }
 }
