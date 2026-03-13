36f1ee25d81288be80abbf1a96f5c520c3498ccb
Karan Babar <karan.babar2004@gmail.com>
2026-03-13 20:09:18 +0530

    Basics of Linked List

--- LinkedList/Basics.java
+++ LinkedList/Basics.java
@@ -13,10 +13,12 @@ public class Basics {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data){
         //Create new node
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -30,6 +32,7 @@ public class Basics {
     public void addLast(int data){
         //Create new node
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -37,6 +40,50 @@ public class Basics {
         
         tail.next = newNode; //Link
         tail = newNode;
+    }
+
+    // Remove from start
+    public void removeFirst() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        } else if(size == 1){
+            head = tail = null;
+            size--;
+            return;
+        }
+
+        head = head.next;
+        size--;
+    }
+
+    // Remove from end
+    public void removeLast() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        } else if(size == 1){
+            head = tail = null;
+            size--;
+            return;
+        }
+
+        if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+
+        Node secondLast = head;
+        Node last = head.next;
+
+        while (last.next != null) {
+            last = last.next;
+            secondLast = secondLast.next;
+        }
+
+        secondLast.next = null;
+        size--;
     }
 
     public void print(){
@@ -55,11 +102,17 @@ public class Basics {
     public static void main(String[] args) {
         Basics ll = new Basics();
         ll.print();
+        ll.addFirst(3);
         ll.addFirst(2);
         ll.addFirst(1);
-        ll.addLast(3);
         ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
 
+        ll.removeFirst();
+        ll.removeLast();
+         
         ll.print();
+        System.out.println(ll.size);
     }
 }
